9c293a436a67972ac4d752da775eb4fec24fcfd8
susu <u1240976@gmail.com>
2014-06-24 01:26:02 +0800

    add testcase multiStmtFor.c

---
+++ testcase/codeGen2/multiStmtFor.c
@@ -0,0 +1,16 @@
+int main (){
+
+    int i,j;
+    for(i=5,j=0 ; i<5,j<5 ; i = i + 1, j = j + 1){
+        write(j);
+        write("\n");
+    }
+
+    /*
+    0
+    1
+    2
+    3
+    4
+    */
+}
